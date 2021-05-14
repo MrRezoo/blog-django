e9a8a84557d5c050190aecb1386eefbaa9c2f16e
MrRezoo <rezam578@gmail.com>
2021-05-14 17:16:03 +0430

    article model builded

--- blog_app/models.py
+++ blog_app/models.py
@@ -14,7 +14,10 @@ class Articles(models.Model):
     slug = models.SlugField(max_length=120, unique=True)
     writer = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
-    publish = models.DateTimeField(default=timezone.now())
+    publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=20, choices=STATUS, default='draft')
+
+    def __str__(self):
+        return f"{self.title} #{self.writer}"
