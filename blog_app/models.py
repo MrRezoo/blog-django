4d7447196ef85844a84705feca1edda4c9626b18
MrRezoo <rezam578@gmail.com>
2021-05-14 16:53:35 +0430

    article model builded

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+from django.utils import timezone
+
+
+class Articles(models.Model):
+    STATUS = (
+        ('draft', 'Draft'),
+        ('pub;isj', 'Publish')
+    )
+    title = models.CharField(max_length=120)
+    slug = models.SlugField(max_length=120, unique=True)
+    writer = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now())
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=20, choices=STATUS, default='draft')
