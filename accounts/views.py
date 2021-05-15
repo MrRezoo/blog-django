46659ba93a29209489d4f0f58596cffd7014e252
MrRezoo <rezam578@gmail.com>
2021-05-15 17:57:13 +0430

    login form

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .forms import UserLoginForm
 
 
 # Create your views here.
 def user_login(request):
-    return HttpResponse('Login form')
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        print(form.is_valid())
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+    else:
+        form = UserLoginForm()
+    return render(request, 'accounts/login.html', {'form': form})
--- blog_app/models.py
+++ blog_app/models.py
@@ -31,5 +31,4 @@ class Article(models.Model):
 
     def get_absolute_url(self):
         from django.urls import reverse
-
         return reverse('blog_app:article_detail', args=[self.id, self.slug])
