ad55ad14c787011451e3bd8558b4cf97a4f5743c
MrRezoo <rezam578@gmail.com>
2021-05-16 04:27:24 +0430

    clean_email

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,30 @@
+from inspect import getmembers
+
 from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.template.defaultfilters import pprint
 
 
 class UserLoginForm(forms.Form):
     username = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'class': 'form-control', 'type': 'text'}))
-    password = forms.CharField(max_length=50,
-                               widget=forms.PasswordInput(attrs={'class': 'form-control', 'type': 'password'
-                                                                                                  ''}))
+    password = forms.CharField(max_length=50, widget=forms.PasswordInput(attrs={'class': 'form-control',
+                                                                                'type': 'password'}))
+
+
+class UserRegistrationForm(forms.Form):
+    username = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'class': 'form-control',
+                                                                            'placeholder': 'enter own username'}))
+
+    email = forms.EmailField(max_length=50, widget=forms.EmailInput(attrs={'class': 'form-control', 'type': 'email'}))
+
+    password = forms.CharField(max_length=50, widget=forms.PasswordInput(attrs={'class': 'form-control',
+                                                                                'type': 'password'}))
+
+    def clean_email(self):
+        # var_dump(self.cleaned_data)
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('This email already exists')
+            # raise ValidationError('This email already exists')
+        return email
--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,14 @@
 import json
 
+import var_dump
 from django.contrib import messages
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.template.defaultfilters import pprint
 
-from .forms import UserLoginForm
+from .forms import UserLoginForm, UserRegistrationForm
 
 
 # Create your views here.
@@ -26,3 +28,23 @@ def user_login(request):
     else:
         form = UserLoginForm()
     return render(request, 'accounts/login.html', {'form': form})
+
+
+def user_register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            User.objects.create_user(data['username'], data['email'], data['password'])
+            messages.success(request, 'you registered successfully, please login', 'success')
+            return redirect('accounts:user_login')
+
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'accounts/register.html', {'form': form})
+
+
+def user_logout(request):
+    logout(request)
+    messages.success(request, 'you logout successfully', 'success')
+    return redirect('blog_app:all_articles')
